fdf14ad0ef1b4102128056e35fccc5981a32b1a6
joaomcsferreira <marcosc024@gmail.com>
2023-08-20 20:52:30 -0300

    feat: Added basic style configurations and necessary image files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        mysticOrchid: "#8642F4",
+        azureDream: "#1574F3",
+        skySerenade: "#0B95E9",
+        aquaBreeze: "#0B9ED2",
+        tealTranquil: "#13AEBC",
+        emeraldWhisper: "#00C9B7",
+        obsidian: "#181A1B",
+        gainsboro: "#9E9E9E",
+        white: "#fff",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        blueVioletAurora:
+          "linear-gradient(45deg, #8642F4, #1574F3, #0B95E9, #0B9ED2, #13AEBC, #00C9B7);",
+      },
+      gridTemplateColumns: {
+        3070: "30% 70%",
       },
     },
   },
